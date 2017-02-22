0a75fbe34bb34cd4da646335e97adabaffc09cbb
Anthony <stenbergdigeronimo@gmail.com>
2017-02-22 12:26:25 -0800

    Assignment3
    Classic ADO

---
+++ Assignment3Example/App_Code/loginClass.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data; // general data tools
+using System.Data.SqlClient; // SQL SErver data tools
+using System.Configuration; // tools for talking to the config file
+
+/// <summary>
+/// Summary description for loginClass
+/// </summary>
+public class LoginClass
+{
+
+    SqlConnection connect;
+    string user;
+    string pass;
+
+    public LoginClass(string username, string password)
+    {
+
+        user = username;
+        pass = password;
+        string connectString = ConfigurationManager.ConnectionStrings["CommunityConnection"].ToString();
+        connect = new SqlConnection(connectString);
+    }
+
+    public int ValidateLogin()
+    {
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = connect;
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.CommandText = "usp_Login";
+
+        cmd.Parameters.AddWithValue("@Email", user);
+        cmd.Parameters.AddWithValue("@password", pass);
+
+        connect.Open();
+        int result = cmd.ExecuteNonQuery();
+        connect.Close();
+
+        int key = 0;
+
+        if (result != -1)
+        {
+            key = GetUserkey();
+        }
+        return key;
+    }
+
+
+
+    private int GetUserkey()
+    {
+        string sql = "Select PersonKey from Person where PersonEmail = @Email";
+        SqlCommand cmd = new SqlCommand(sql, connect);
+        cmd.Parameters.AddWithValue("@Email", user);
+        SqlDataReader reader = null;
+        connect.Open();
+        reader = cmd.ExecuteReader();
+
+        int key = 0;
+
+        while (reader.Read())
+        {
+            key = (int)reader["PersonKey"];
+        }
+
+        connect.Close();
+        return key;
+
+    }
+
+}
---
+++ Assignment3Example/GrantRequests.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+
+public partial class GrantRequests : System.Web.UI.Page
+{
+
+    GrantRequestClass gr = new GrantRequestClass();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["UserKey"] != null)
+        {
+            if (!IsPostBack)
+            {
+                DataTable table = gr.GetGrantTitles();
+                DropDownList1.DataSource=table;
+                DropDownList1.DataTextField = "GrantTypeName";
+                DropDownList1.DataValueField = "GrantTypeKey";
+                DropDownList1.DataBind();
+                ListItem item = new ListItem("Pick a grant:");
+                DropDownList1.Items.Insert(0, item);
+            }
+        }
+
+        else
+        {
+            Response.Redirect("Default.aspx");
+        }
+    }
+
+    protected void FillGrid()
+    {
+        if (!DropDownList1.SelectedItem.Text.Equals("Pick a grant:"))
+        {
+            int key = int.Parse(DropDownList1.SelectedValue.ToString());
+            DataTable tbl = gr.GetGrants(key);
+            GridView1.DataSource = tbl;
+            GridView1.DataBind();
+        }
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        FillGrid();
+    }
+
+}
